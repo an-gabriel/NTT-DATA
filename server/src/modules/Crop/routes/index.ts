7e8730f4c440be825dc8fae45eea6e0346ec8399
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-11-20 01:30:46 -0300

    chore: add crop expor routes

--- server/src/modules/Crop/routes/index.ts
+++ server/src/modules/Crop/routes/index.ts
@@ -1,12 +1,12 @@
 /**
  * @swagger
  * tags:
- *   name: Farmers
+ *   name: Crops
  *   description: API para manipulação de dados de agricultores
  */
 
-export * from '@/modules/Farmer/routes/GET/All'
-export * from '@/modules/Farmer/routes/GET/ByFilters'
-export * from '@/modules/Farmer/routes/POST'
-export * from '@/modules/Farmer/routes/PUT'
-export * from '@/modules/Farmer/routes/DELETE'
+export * from '@/modules/Crop/routes/GET/All'
+export * from '@/modules/Crop/routes/GET/ByFilters'
+export * from '@/modules/Crop/routes/POST'
+export * from '@/modules/Crop/routes/PUT'
+export * from '@/modules/Crop/routes/DELETE'
