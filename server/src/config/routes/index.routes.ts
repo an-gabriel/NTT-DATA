e2e8ad7efb80d593fb824a2a1e6241cd8954f60d
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-11-20 01:31:28 -0300

    chore: add routes

--- server/src/config/routes/index.routes.ts
+++ server/src/config/routes/index.routes.ts
@@ -1,22 +1,34 @@
 import express from 'express';
-import getAll from '@/modules/Farmer/routes/GET/All';
+
+import getAllFarmer from '@/modules/Farmer/routes/GET/All';
 import createFarmer from '@/modules/Farmer/routes/POST';
 import updateFarmer from '@/modules/Farmer/routes/PUT';
 import deleteFarmer from '@/modules/Farmer/routes/DELETE';
 import getFarmersByFilters from '@/modules/Farmer/routes/GET/ByFilters';
 
+import getAllCrop from '@/modules/Crop/routes/GET/All';
+import createCrop from '@/modules/Crop/routes/POST';
+import updateCrop from '@/modules/Crop/routes/PUT';
+import deleteCrop from '@/modules/Crop/routes/DELETE';
+import getCropByFilters from '@/modules/Crop/routes/GET/ByFilters';
+
 const AllRoutes = express.Router();
 
-AllRoutes.use('/farmers', getAll);
+AllRoutes.use((req, res, next) => {
+    console.log(req.url);
+    next()
+})
+
+AllRoutes.use('/farmers', getAllFarmer);
 AllRoutes.use('/farmers/create', createFarmer);
 AllRoutes.use('/farmers/update', updateFarmer);
 AllRoutes.use('/farmers/delete', deleteFarmer);
 AllRoutes.use('/farmers/filters', getFarmersByFilters);
 
-AllRoutes.use('/crops', getAll);
-AllRoutes.use('/crops/create', createFarmer);
-AllRoutes.use('/crops/update', updateFarmer);
-AllRoutes.use('/crops/delete', deleteFarmer);
-AllRoutes.use('/crops/filters', getFarmersByFilters);
+AllRoutes.use('/crops', getAllCrop);
+AllRoutes.use('/crops/create', createCrop);
+AllRoutes.use('/crops/update', updateCrop);
+AllRoutes.use('/crops/delete', deleteCrop);
+AllRoutes.use('/crops/filters', getCropByFilters);
 
 export default AllRoutes;
