fb30866ebd74b918c8cf41df7d9ded4f183e19ef
Gabriel Aquino <aquino.gabriel@outlook.com.br>
2023-11-19 22:15:22 -0300

    chore: add CropsByFiltersRouter

---
+++ server/src/modules/Crop/routes/GET/ByFilters/index.ts
@@ -0,0 +1,107 @@
+import express, { Request, Response, Router } from 'express';
+import CropController from '@/modules/Crop/controllers/Crop.controllers';
+import ValidationMiddleware from '@/Middleware/ValidateSchema';
+import { CropQueryParams } from '@/modules/Crop/interfaces';
+import { CropQueryParamsSchema } from '@/modules/Crop/schemas/CropQueryParamsSchema';
+
+/**
+ * @swagger
+ * /api/v1/crops/filters:
+ *   get:
+ *     summary: Retorna platanção com base em filtros
+ *     tags: [Crops]
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         description: Nome da platanção
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: startDate
+ *         description: Data de início da platanção
+ *         schema:
+ *           type: string
+ *           format: date
+ *       - in: query
+ *         name: endDate
+ *         description: Data de término da platanção
+ *         schema:
+ *           type: string
+ *           format: date
+ *       - in: query
+ *         name: active
+ *         description: Indica se a platanção está ativa
+ *         schema:
+ *           type: boolean
+ *     responses:
+ *       200:
+ *         description: Lista de platanção filtrada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     format: uuid
+ *                     description: UUID da platanção
+ *                   name:
+ *                     type: string
+ *                     description: Nome da platanção
+ *                   startDate:
+ *                     type: string
+ *                     format: date
+ *                     description: Data de início da platanção
+ *                   endDate:
+ *                     type: string
+ *                     format: date
+ *                     description: Data de término da platanção
+ *                   active:
+ *                     type: boolean
+ *                     description: Indica se a platanção está ativa
+ *               example:
+ *                 - id: "ac9e6e3e-8f1b-4fc1-bb63-d4e57f1b3f8f"
+ *                   name: "platanção 1"
+ *                   startDate: "2023-01-01"
+ *                   endDate: "2023-12-31"
+ *                   active: true
+ *                 - id: "ecdf25d2-5eab-4a6a-9b18-4c0380d602f1"
+ *                   name: "platanção 2"
+ *                   startDate: "2023-02-01"
+ *                   endDate: "2023-11-30"
+ *                   active: false
+ *       500:
+ *         description: Erro interno do servidor
+ */
+class CropsByFiltersRouter {
+    public router: Router = express.Router();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    private async getCropsByFilters(req: Request, res: Response): Promise<void> {
+        try {
+            const filters: CropQueryParams = req.query;
+            const crops = await CropController.getCropsByFilters(filters);
+            res.status(200).json(crops);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+        }
+    }
+
+    private initializeRoutes(): void {
+        this.router.get(
+            '/',
+            ValidationMiddleware.validateSchema(CropQueryParamsSchema),
+            this.getCropsByFilters.bind(this)
+        );
+    }
+}
+
+const cropsByFiltersRouter = new CropsByFiltersRouter();
+
+export default cropsByFiltersRouter.router;
